3bbf53de234b4cfb1753c0bdae483a88095c53ce
Lucas Serafim <lucas.serafim@fornecedoramaquinas.com.br>
2021-09-06 16:32:44 -0300

    [Gameplay Aula-01] Aula 01 completa

---
+++ src/screens/SignIn/index.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { View, Text, Image, StatusBar } from "react-native";
+import { ButtonIcon } from "../../components/ButtonIcon";
+import { styles } from './styles'
+import IllustrationImg from '../../assets/images/illustration.png';
+
+export function SignIn() {
+  
+  return(
+    <View style={styles.container}>
+      <StatusBar 
+        barStyle='light-content'
+        backgroundColor='transparent'
+        translucent
+      />
+      <Image 
+        source={IllustrationImg}
+        style={styles.image}
+        resizeMode="stretch"
+      />
+
+      <View style={styles.content}>
+        <Text style={styles.title}>
+          Organize {`\n`}
+          suas jogatinas {`\n`}
+          facilmente
+        </Text>
+
+        <Text style={styles.subtitle}>
+          Crie grupos para jogar seus games {`\n`}
+          favoritos com seus amigos
+        </Text>
+
+        <ButtonIcon
+          title="Entre com Discord"
+          activeOpacity={0.7}
+        />
+      </View>
+    </View>
+  )
+}
