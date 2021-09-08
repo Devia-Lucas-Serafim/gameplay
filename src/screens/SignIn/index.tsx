e402798838532af8db6a41973d3b1737a8a873b4
Lucas Serafim <lucas.serafim@fornecedoramaquinas.com.br>
2021-09-08 16:01:38 -0300

    [Gameplay Aula-02] Aula 02 em progreço (49%)

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Text, Image, StatusBar } from "react-native";
+import { View, Text, Image } from "react-native";
 import { ButtonIcon } from "../../components/ButtonIcon";
 import { styles } from './styles'
 import IllustrationImg from '../../assets/images/illustration.png';
@@ -8,11 +8,7 @@ export function SignIn() {
   
   return(
     <View style={styles.container}>
-      <StatusBar 
-        barStyle='light-content'
-        backgroundColor='transparent'
-        translucent
-      />
+      
       <Image 
         source={IllustrationImg}
         style={styles.image}
@@ -21,13 +17,13 @@ export function SignIn() {
 
       <View style={styles.content}>
         <Text style={styles.title}>
-          Organize {`\n`}
-          suas jogatinas {`\n`}
-          facilmente
+          Conecte-se {'\n'}
+          e organize suas {'\n'}
+          jogatinas
         </Text>
 
         <Text style={styles.subtitle}>
-          Crie grupos para jogar seus games {`\n`}
+          Crie grupos para jogar seus games {'\n'}
           favoritos com seus amigos
         </Text>
 
